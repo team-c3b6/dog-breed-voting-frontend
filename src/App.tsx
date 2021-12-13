0550624722e5769c56b3cd6f616ab54345315042
Katarzyna Dziewicka <dziewicka.k@gmail.com>
2021-12-13 15:50:50 +0000

    Define all component files and add components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { greet } from "./utils/greet";
+import Header from "./components/Header";
+import Voting from "./components/Voting";
+import Leaderboard from "./components/Leaderboard";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return <>
+    <Header />
+    <Voting />
+    <Leaderboard />
+  </>;
 }
 
 export default App;
---
+++ src/components/TopThree.tsx
@@ -0,0 +1,3 @@
+export default function TopThree(): JSX.Element {
+    return <></>
+};
