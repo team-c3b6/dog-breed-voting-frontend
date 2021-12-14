9954733b72c8a3effd1c3ebd7185536d07261a29
Katarzyna Dziewicka <dziewicka.k@gmail.com>
2021-12-14 16:00:15 +0000

    Fetch data in leadership component

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,10 +1,24 @@
 import IBreed from "../utils/IBreed";
+// import TopTen from "./TopTen";
+import { useEffect } from "react";
+import axios from "axios";
 
 interface LeaderboardProps {
-  breedList: IBreed[];
-  setBreedList: (breedList: IBreed[]) => void;
+    breedList: IBreed[];
+    setBreedList: (breedList: IBreed[]) => void
 }
 
 export default function Leaderboard(props: LeaderboardProps): JSX.Element {
-  return <></>;
+    async function fetchBreeds() {
+        await axios.get("https://dog-breed-voting-c3b6.herokuapp.com/breeds")
+        .then((response) => {
+            console.log(response);
+            props.setBreedList(response.data.data.breedsList)
+          })
+    }
+    useEffect(() => {fetchBreeds();}, []);
+
+    console.log(props.breedList)
+  
+    return <></>;
 }
