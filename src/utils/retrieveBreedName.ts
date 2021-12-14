55bc208c118178a41b41cd21d47793f5e62881a7
David Culemann <davidculemann@gmail.com>
2021-12-14 16:48:51 +0400

    format with prettier

--- src/utils/retrieveBreedName.ts
+++ src/utils/retrieveBreedName.ts
@@ -1,3 +1,3 @@
 export function retrieveBreedName(url: string) {
-    return url.slice(url.indexOf("breeds") + 7, url.lastIndexOf("/"))
+  return url.slice(url.indexOf("breeds") + 7, url.lastIndexOf("/"));
 }
