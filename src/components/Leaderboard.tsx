42205230f9be4f990e9d7d85904936929412145f
David Culemann <davidculemann@gmail.com>
2021-12-14 16:07:19 +0400

    passed breedList state down to leaderboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,3 +1,10 @@
-export default function Leaderboard(): JSX.Element {
+import IBreed from "../utils/IBreed";
+
+interface LeaderboardProps {
+  breedList: IBreed[];
+  setBreedList: (breedList: IBreed[]) => void;
+}
+
+export default function Leaderboard(props: LeaderboardProps): JSX.Element {
   return <></>;
 }
