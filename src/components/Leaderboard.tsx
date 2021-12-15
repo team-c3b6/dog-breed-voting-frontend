9fb66d9dd3882e48eda018e9de4c79241c639970
David Culemann <davidculemann@gmail.com>
2021-12-15 21:15:59 +0400

    add refresh button to update the breedlist state

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -3,6 +3,7 @@ import TopTen from "./TopTen";
 import TopThree from "./TopThree";
 import { useEffect } from "react";
 import axios from "axios";
+import { APIbase } from "../utils/APIbase";
 
 interface LeaderboardProps {
   breedList: IBreed[];
@@ -11,19 +12,15 @@ interface LeaderboardProps {
 
 export default function Leaderboard(props: LeaderboardProps): JSX.Element {
   async function fetchBreeds() {
-    await axios
-      .get("https://dog-breed-voting-c3b6.herokuapp.com/breeds")
-      .then((response) => {
-        console.log(response);
-        props.setBreedList(response.data.data.breedsList);
-      });
+    await axios.get(APIbase + "/breeds").then((response) => {
+      console.log(response);
+      props.setBreedList(response.data.data.breedsList);
+    });
   }
   useEffect(() => {
     fetchBreeds();
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  console.log(props.breedList);
 
   return (
     // <>
@@ -34,15 +31,20 @@ export default function Leaderboard(props: LeaderboardProps): JSX.Element {
     //     <TopTen breedList={props.breedList} />
     //   </div>
     // </>
-    <div className="container" text-align="center">
+    <div className="container pb-2" text-align="center">
       <div className="row">
         <div className="col-9">
-          {" "}
           <TopThree breedList={props.breedList} />
         </div>
         <div className="col-3">
-          {" "}
           <TopTen breedList={props.breedList} />
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={fetchBreeds}
+          >
+            refresh
+          </button>
         </div>
       </div>
     </div>
