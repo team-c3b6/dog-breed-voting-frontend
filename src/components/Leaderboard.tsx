3c3d16c80385579e8d13a507cc05292db96c0987
Katarzyna Dziewicka <dziewicka.k@gmail.com>
2021-12-15 11:43:42 +0000

    Formatted with prettier, eslint ignore a warning (concerns useEffect dependencies list)

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -4,21 +4,25 @@ import { useEffect } from "react";
 import axios from "axios";
 
 interface LeaderboardProps {
-    breedList: IBreed[];
-    setBreedList: (breedList: IBreed[]) => void
+  breedList: IBreed[];
+  setBreedList: (breedList: IBreed[]) => void;
 }
 
 export default function Leaderboard(props: LeaderboardProps): JSX.Element {
-    async function fetchBreeds() {
-        await axios.get("https://dog-breed-voting-c3b6.herokuapp.com/breeds")
-        .then((response) => {
-            console.log(response);
-            props.setBreedList(response.data.data.breedsList)
-          })
-    }
-    useEffect(() => {fetchBreeds();}, []);
+  async function fetchBreeds() {
+    await axios
+      .get("https://dog-breed-voting-c3b6.herokuapp.com/breeds")
+      .then((response) => {
+        console.log(response);
+        props.setBreedList(response.data.data.breedsList);
+      });
+  }
+  useEffect(() => {
+    fetchBreeds();
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    console.log(props.breedList)
-  
-    return <></>;
+  console.log(props.breedList);
+
+  return <></>;
 }
