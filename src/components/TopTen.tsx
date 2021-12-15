2233e34638f20cb618a80815b0c1f4452c0c19f8
Truman Tong <trumanskt@gmail.com>
2021-12-16 00:07:10 +0800

    added top10 component functionality

--- src/components/TopTen.tsx
+++ src/components/TopTen.tsx
@@ -1,14 +1,33 @@
 import IBreed from "../utils/IBreed";
+import changeName from "../utils/changeName";
 
-export default function TopTen(props: IBreed): JSX.Element {
+interface TopTenProps {
+  breedList: IBreed[];
+}
+
+export default function TopTen(props: TopTenProps): JSX.Element {
+  const topTen = props.breedList.slice(0, 10);
+
   return (
-    <>
-      <table>
-        <tr>
-          <td>{props.breed}</td>
-          <td>{props.votes}</td>
-        </tr>
-      </table>
-    </>
+    <div className="container" text-align="left">
+      <div className="table">
+        <div className="thead">
+          <div className="trow">
+            <th scope="col">Rank</th>
+            <th scope="col">Breed</th>
+            <th scope="col">Votes</th>
+          </div>
+        </div>
+        <div className="tbody">
+          {topTen.map((e, index) => (
+            <div key={e.id} className="trow">
+              <td>{index + 1}</td>
+              <td>{changeName(e.breed)}</td>
+              <td>{e.votes}</td>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
