82cd4d9306fa3a7f9b8120018d88a623870979d1
David Culemann <davidculemann@gmail.com>
2021-12-16 20:53:08 +0400

    Added bark sound to voting and leaderboard table style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header(): JSX.Element {
   return (
-    <div className="p-2">
+    <div className="mx-5 pt-4 pb-4 text-align-center">
       <h1>🌭Hottest Dogs🌭</h1>
       <h3>Pick your favourite breed!</h3>
       <h5>Click on the picture of a breed you like best</h5>
