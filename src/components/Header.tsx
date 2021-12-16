d6ee59ca2219c245f20d973ed9bd4ced03d1d444
Katarzyna Dziewicka <dziewicka.k@gmail.com>
2021-12-16 15:56:40 +0000

    Add subheadings to TopThree and TopTen components; change the main heading to Hottest Dogs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 export default function Header(): JSX.Element {
   return (
     <div className="p-2">
-      <h1>Dog Breed Voting</h1>
+      <h1>🌭Hottest Dogs🌭</h1>
       <h3>Pick your favourite breed!</h3>
+      <h5>Click on the picture of a breed you like best</h5>
     </div>
   );
 }
